80d4fe346e1810cf2f80012a502da33e9982b568
Orion <9056620+orion-git@users.noreply.github.com>
2023-12-07 00:25:00 +0000

    Delete src/main/java/com/jonathang/TickFixerConfig.java

--- src/main/java/com/jonathang/TickFixerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.jonathang;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
-
-@ConfigGroup("example")
-public interface TickFixerConfig extends Config {
-}
