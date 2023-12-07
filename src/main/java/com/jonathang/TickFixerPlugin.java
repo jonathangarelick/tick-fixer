d1afc92a5f57b07cb0ae032c544f3341541e6e1b
Orion <9056620+orion-git@users.noreply.github.com>
2023-12-07 00:25:23 +0000

    Update TickFixerPlugin.java

--- src/main/java/com/jonathang/TickFixerPlugin.java
+++ src/main/java/com/jonathang/TickFixerPlugin.java
@@ -1,15 +1,9 @@
 package com.jonathang;
 
-import com.google.inject.Provides;
-
-import javax.inject.Inject;
-
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.Client;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import org.apache.commons.lang3.SystemUtils;
+import net.runelite.client.util.OSType;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,82 +12,79 @@ import java.net.InetAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
-@PluginDescriptor(
-        name = "Tick Fixer for Mac"
-)
+@PluginDescriptor(name = "Tick Fixer for Mac")
 public class TickFixerPlugin extends Plugin {
-    private static final String[] cmd = {
+    private static final String[] CMD = {
             "/bin/zsh",
             "-c",
             "netstat -nr | awk '/default/ {print $2}' | head -n1"
     };
+    private static final int MAX_FAILURES = 10;
+    private static final int PING_INTERVAL = 200; // in milliseconds
 
-    private final AtomicBoolean isGatewayReachable = new AtomicBoolean(false);
     private final AtomicInteger failureCount = new AtomicInteger(0);
-
-	private ScheduledExecutorService exec;
-
-    @Inject
-    private Client client;
-
-    @Inject
-    private TickFixerConfig config;
+    private ScheduledExecutorService exec;
 
     @Override
     protected void startUp() throws Exception {
         log.info("Tick Fixer for Mac started.");
 
-        if (!SystemUtils.IS_OS_MAC) {
+        if (OSType.getOSType() != OSType.MacOS) {
             log.error("Operating system is not Mac. Terminating.");
             return;
         }
 
-        log.info("Getting default gateway address.");
-        Process pr = Runtime.getRuntime().exec(cmd);
+        InetAddress address = getDefaultGatewayAddress();
+        if (address == null) return;
 
-        BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-        final InetAddress address = InetAddress.getByName(input.readLine());
+        log.debug("Default gateway is " + address);
+        exec = Executors.newSingleThreadScheduledExecutor();
+        exec.scheduleAtFixedRate(this::pingGateway, 0, PING_INTERVAL, TimeUnit.MILLISECONDS);
+    }
 
-        if (address.isLoopbackAddress()) {
-            log.error("Default gateway not found. Terminating.");
+    private InetAddress getDefaultGatewayAddress() throws IOException {
+        log.debug("Getting default gateway address.");
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec(CMD).getInputStream()))) {
+            final InetAddress address = InetAddress.getByName(input.readLine());
+
+            if (address.isLoopbackAddress()) {
+                log.error("Default gateway not found. Terminating.");
+                return null;
+            }
+
+            return address;
+        }
+    }
+
+    private void pingGateway() {
+        if (failureCount.get() >= MAX_FAILURES) {
+            log.error("Failed to ping default gateway 10 times. Terminating.");
+            exec.shutdown();
             return;
         }
 
-        log.info("Default gateway is " + address);
-
-		exec = Executors.newSingleThreadScheduledExecutor();
-        exec.scheduleAtFixedRate(() -> {
-            if (failureCount.get() >= 10) {
-                log.error("Failed to ping default gateway 10 times. Terminating.");
-                exec.shutdown();
-            }
-
-            try {
-                isGatewayReachable.set(address.isReachable(150));
-
-                if (isGatewayReachable.get())
-                    failureCount.set(0);
-                else
-                    failureCount.getAndIncrement();
-            } catch (IOException e) {
-                log.error(e.getMessage());
+        try {
+            InetAddress address = getDefaultGatewayAddress();
+            boolean isReachable = address != null && address.isReachable(150);
+            if (isReachable) {
+                failureCount.set(0);
+            } else {
                 failureCount.getAndIncrement();
             }
-        }, 0, 200, TimeUnit.MILLISECONDS);
+        } catch (IOException e) {
+            log.error("Error pinging gateway: " + e.getMessage());
+            failureCount.getAndIncrement();
+        }
     }
 
     @Override
     protected void shutDown() throws Exception {
-        exec.shutdown();
+        if (exec != null && !exec.isShutdown()) {
+            exec.shutdown();
+        }
         log.info("Tick Fixer for Mac stopped.");
-    }
-
-    @Provides
-    TickFixerConfig provideConfig(ConfigManager configManager) {
-        return configManager.getConfig(TickFixerConfig.class);
     }
 }
