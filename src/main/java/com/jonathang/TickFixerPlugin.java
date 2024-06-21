9983d32c62c767c3083c6496cf4250062b89e3f2
Blake Felt <blake.w.felt@gmail.com>
2024-06-20 23:47:55 -0600

    Fix for restarting plugin

--- src/main/java/com/jonathang/TickFixerPlugin.java
+++ src/main/java/com/jonathang/TickFixerPlugin.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -17,24 +18,25 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 @PluginDescriptor(name = "Tick Fixer for Mac")
 public class TickFixerPlugin extends Plugin {
-    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private ScheduledExecutorService executor;
     private final AtomicInteger failureCount = new AtomicInteger(0);
 
     @Override
     protected void startUp() {
-        log.info("Tick Fixer v1.0.3 started"); // Remember to update build.gradle when changing version
+        log.info("Tick Fixer v1.0.4 started"); // Remember to update build.gradle when changing version
 
         if (OSType.getOSType() != OSType.MacOS) {
             log.error("Operating system is not Mac. Terminating.");
             return;
         }
 
+        createExecutor();
         startPing();
     }
 
     @Override
     protected void shutDown() {
-        executor.shutdown();
+        shutdownExecutor();
         log.info("Tick Fixer stopped");
     }
 
@@ -92,5 +94,17 @@ public class TickFixerPlugin extends Plugin {
                 log.error(e.getMessage());
             }
         }, 0, 200, TimeUnit.MILLISECONDS);
+    }
+
+    private void createExecutor() {
+        if (Objects.isNull(executor) || executor.isShutdown()) {
+            executor = Executors.newSingleThreadScheduledExecutor();
+        }
+    }
+
+    private void shutdownExecutor() {
+        if (Objects.nonNull(executor)) {
+            executor.shutdown();
+        }
     }
 }
